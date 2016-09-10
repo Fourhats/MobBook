f1439b0d74d87b16cba2ec880cf2c0b545a344b1
pedrofourhats <pedro@fourhats.com.ar>
2016-09-10 12:45:08 -0300

    Animaciones para als pantallas y nueva pagina (goToPage)

--- www/js/app.js
+++ www/js/app.js
@@ -65,6 +65,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 		templateUrl: 'templates/microphono.html'
 	})
 
+		.state('goToPage', {
+		url: '/goToPage',
+		controller: "GotopageCtrl",
+		templateUrl: 'templates/goToPage.html'
+	})
+
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/home');
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,51 +1,74 @@
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function($scope) {
-})
+	.controller('HomeCtrl', function ($scope) {
+	})
 
-.controller('MenuCtrl', function($scope) {
-})
+	.controller('MenuCtrl', function ($scope) {
+	})
 
-.controller('VolumenUpCtrl', function($scope) {
-	document.removeEventListener("volumeupbutton", onVolumeUpKeyDown);
-	document.removeEventListener("volumedownbutton", onVolumeDownKeyDown);
-	window.removeEventListener("shake", onShake);
-	document.addEventListener("volumeupbutton", onVolumeUpKeyDown, false);
-})
+	.controller('VolumenUpCtrl', function ($scope) {
+		document.removeEventListener("volumeupbutton", onVolumeUpKeyDown);
+		document.removeEventListener("volumedownbutton", onVolumeDownKeyDown);
+		window.removeEventListener("shake", onShake);
+		document.addEventListener("volumeupbutton", onVolumeUpKeyDown, false);
+	})
 
-.controller('VolumenDownCtrl', function($scope) {
-	document.removeEventListener("volumeupbutton", onVolumeUpKeyDown);
-	document.removeEventListener("volumedownbutton", onVolumeDownKeyDown);
-	window.removeEventListener("shake", onShake);
-	document.addEventListener("volumedownbutton", onVolumeDownKeyDown, false);
-})
+	.controller('VolumenDownCtrl', function ($scope, $location) {
+		document.removeEventListener("volumeupbutton", onVolumeUpKeyDown);
+		document.removeEventListener("volumedownbutton", onVolumeDownKeyDown);
+		window.removeEventListener("shake", onShake);
+		document.addEventListener("volumedownbutton", onVolumeDownKeyDown, false);
 
-.controller('ShakeCtrl', function($scope) {
-	document.removeEventListener("volumeupbutton", onVolumeUpKeyDown);
-	document.removeEventListener("volumedownbutton", onVolumeDownKeyDown);
-	window.removeEventListener("shake", onShake);
-	
-	myShakeEvent = new Shake({
-		threshold: 15
+		$scope.class = "animated fadeInLeft";
+
+		$scope.changeClass = function () {
+			$scope.class = "animated fadeOutRight";
+			$location.path('/goToPage')
+
+		};
+	})
+
+	.controller('ShakeCtrl', function ($scope) {
+		document.removeEventListener("volumeupbutton", onVolumeUpKeyDown);
+		document.removeEventListener("volumedownbutton", onVolumeDownKeyDown);
+		window.removeEventListener("shake", onShake);
+
+		myShakeEvent = new Shake({
+			threshold: 15
+		});
+
+
+		//$scope.class = "shake";
+
+		$scope.changeClass = function () {
+			$scope.classUp = "animated fadeOut infinite";
+			$scope.classBottom = "animated pulse infinite";
+		};
+
+
+		// start listening to device motion
+		myShakeEvent.start();
+
+		// register a shake event
+		window.addEventListener('shake', onShake, false);
+	})
+
+	.controller('MicrophonoCtrl', function ($scope) {
+
+		$scope.changeClass = function () {
+			$scope.classUp = "animated bounceOut";
+			setTimeout(function () {
+				$scope.classUp = "";
+				$scope.classUp = "animated bounceIn";
+			}, 2000);
+
+		};
+
+	})
+
+	.controller('GotopageCtrl', function ($scope) {
+
 	});
-
-
-    $scope.class = "shake";
-
-	  $scope.changeClass = function(){
-	  $scope.class = "animated bounceInRight";
-  };
-
-
-	// start listening to device motion
-	myShakeEvent.start();
-
-	// register a shake event
-	window.addEventListener('shake', onShake, false);
-})
-
-.controller('MicrophonoCtrl', function($scope) {
-});
 
 var myShakeEvent;
 function onVolumeUpKeyDown() {
@@ -56,5 +79,5 @@ function onVolumeDownKeyDown() {
 }
 function onShake() {
 	alert('Agitaste!');
-	//$scope.class = "animated bounceInRight";
+	//$scope.class = "animated swing infinite";
 }
